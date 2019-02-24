12b7cad38935ed60304780a94b6fc1830ac2693c
Dima Naumow <dimanaumow123@gmail.com>
2019-02-24 16:45:47 +0300

    the third attempt

--- ArithmeticSequence.cs
+++ ArithmeticSequence.cs
@@ -8,33 +8,21 @@ namespace PadawansTask2
         {
             int sum = 0;
 
-            if(count <= 0)
+            if (count <= 0)
             {
-                throw new ArgumentOutOfRangeException("The Count Of Elements Of The Sequence Cannot Be Less Or Equals Zero.");
-            }
-            
-            if(number == int.MaxValue)
-            {
-                throw new ArgumentOutOfRangeException("The Obtained Result Out Of Range Of Integer Values"); 
+                throw new ArgumentException("The Count Of Elements Of The Sequence Cannot Be Less Or Equals Zero.");
             }
 
-            if(number == int.MinValue)
+            for (int i = 0; i <= count - 1; i++)
             {
-                throw new ArgumentOutOfRangeException("The Obtained Result Out Of Range Of Integer Values."); 
+                checked
+                {
+                    sum += add * i + number;
+
+                }
             }
 
-            int sumNumbers = number * count;
-
-            int sumRemainder = 0; 
-
-            for(int i = 0; i <= count - 1; i++)
-            {
-                sumRemainder += add * i; 
-            }
-
-            sum = sumNumbers + sumRemainder; 
-
-            return sum; 
+            return sum;
         }
     }
 }
